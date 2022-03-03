b2800658ca26c73a2d3ee03d2a2004260bbeb682
dzumita <60666903+dzumita@users.noreply.github.com>
2022-03-03 19:08:59 +0300

    add option for settings

--- pages/SettingsPage.tsx
+++ pages/SettingsPage.tsx
@@ -1,28 +1,59 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Switch, Text, StyleSheet } from 'react-native';
 
 import { colors } from '../constants';
 
 const SettingsPage = () => {
+  const [isTimerOption, setIsTimerOption] = useState(true);
+
+  const handleIsTimerOptionSwitch = () => {
+    setIsTimerOption(!isTimerOption);
+  };
+
   return (
-    <View style={styles.clockPageContainer}>
-      <Text style={styles.text}>Soon...</Text>
+    <View style={styles.settingsPageContainer}>
+      <View style={styles.optionContainer}>
+        <Text style={styles.optionText}>
+          When creating a timer, immediately start it
+        </Text>
+
+        <Switch
+          value={isTimerOption}
+          onChange={handleIsTimerOptionSwitch}
+          {...SwitchStyle}
+        />
+      </View>
     </View>
   );
 };
 
+const SwitchStyle = {
+  thumbColor: colors.white,
+  trackColor: {
+    true: colors.whiteOpacity,
+    false: colors.lightDark,
+  },
+};
+
 const styles = StyleSheet.create({
-  clockPageContainer: {
+  settingsPageContainer: {
     flex: 1,
     backgroundColor: colors.dark,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
   },
-  text: {
-    paddingTop: 10,
-    fontSize: 44,
+  optionContainer: {
+    width: '100%',
+    paddingTop: 15,
+    paddingLeft: 15,
+    paddingRight: 15,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  optionText: {
+    fontSize: 16,
     color: colors.white,
-    textAlign: 'center',
+    width: '70%',
   },
 });
 
